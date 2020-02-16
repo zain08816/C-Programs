d94444c92ac44237d5b2bd02588a7c8f9091f5b8
Zain A. Ali <zain08816@gmail.com>
2020-02-16 10:51:02 -0500

    add palindrome

--- pa1/src/palindrome/palindrome.c
+++ pa1/src/palindrome/palindrome.c
@@ -3,13 +3,64 @@
 #include <string.h>
 #include <ctype.h>
 
-int palindrome(char *s){
+int palindrome(char *string){
 
+    int left = 0;
+    int right = strlen(string) - 1;
+    // printf("left: %d with %c\n", left, string[left]);
+    // printf("right: %d with %c\n", right, string[right]);
 
-    return 0;
+    while (right > left) {
+        // printf("left: %d with %c\n", left, string[left]);
+        // printf("right: %d with %c\n", right, string[right]);
+        
+
+        if (isalpha(string[left]) && isalpha(string[right])) {
+            // puts("both are letters");
+            if (tolower(string[left]) == tolower(string[right])){
+                // puts("both are the same");
+                left += 1;
+                right -= 1;
+                continue;
+            } else {
+                // puts("not a palindrome");
+                return 0;
+            }
+        }
+
+         else if (!isalpha(string[left])) {
+            // puts("left isnt letter");
+            left += 1;
+            continue;
+        }
+
+        else if (!isalpha(string[right])) {
+            // puts("right isnt letter");
+            right -= 1;
+            continue;
+        }
+
+    }
+
+    return 1;
+
 }
 
 int main(int argc, char **argv){
+
+    char *string = argv[1];
+
+
+    if (string != NULL) {
+        if (palindrome(string)) {
+            printf("yes\n");
+        }  else {
+            printf("no\n");
+    }
+
+    }
+
+
 
 
     return 0;
